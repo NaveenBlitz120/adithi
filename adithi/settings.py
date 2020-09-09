91465da669c52403b80a63e5333e7f25525b647a
NaveenBLitz120 <naveenblitz@gmail.com>
2020-09-09 19:17:46 +0530

    9 sep adhithi finzlized

--- adithi/settings.py
+++ adithi/settings.py
@@ -78,12 +78,24 @@ WSGI_APPLICATION = 'adithi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {   
+# DATABASES = {   
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
