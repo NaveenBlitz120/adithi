6f6c62b89ffb279a217068ade52e85daa186f553
NaveenBLitz120 <naveenblitz@gmail.com>
2020-09-09 18:11:10 +0530

    minor bugs 9 sep

--- adithi/settings.py
+++ adithi/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'adithi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
+DATABASES = {   
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
